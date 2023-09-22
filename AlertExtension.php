1f14441df1fe07c1027f7849855d92f6bc04cb5b
amoschou <4948673+amoschou@users.noreply.github.com>
2023-09-22 23:37:13 +0930

    Create AlertExtension.php

---
+++ AlertExtension.php
@@ -0,0 +1,10 @@
+use League\CommonMark\Extension\ExtensionInterface;
+use League\CommonMark\Environment\ConfigurableEnvironmentInterface;
+
+final class EmojiExtension implements ExtensionInterface
+{
+    public function register(ConfigurableEnvironmentInterface $environment): void
+    {
+        
+    }
+}
