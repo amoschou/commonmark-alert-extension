aa5846c0d7dbf54d2f6fb54d24f6ac7c53d06188
amoschou <4948673+amoschou@users.noreply.github.com>
2023-09-23 12:44:45 +0930

    Update AlertExtension.php

--- src/AlertExtension.php
+++ src/AlertExtension.php
@@ -5,12 +5,12 @@ namespace AMoschou\CommonMark\Alert;
 use League\CommonMark\Extension\ExtensionInterface;
 use League\CommonMark\Environment\ConfigurableEnvironmentInterface;
 
-final class EmojiExtension implements ExtensionInterface
+final class AlertExtension implements ExtensionInterface
 {
     public function register(ConfigurableEnvironmentInterface $environment): void
     {
-        
+        $environment
+            ->addInlineParser(new AlertParser(), 20)
+            ->addInlineRenderer(Alert::class, new AlertRenderer(), 0);
     }
 }
-
-
