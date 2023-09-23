f78a6193b967690397433a39369d8f182d27af7c
amoschou <4948673+amoschou@users.noreply.github.com>
2023-09-23 16:03:18 +0930

    Update AlertExtension.php

--- src/AlertExtension.php
+++ src/AlertExtension.php
@@ -3,14 +3,14 @@
 namespace AMoschou\CommonMark\Alert;
 
 use League\CommonMark\Extension\ExtensionInterface;
-use League\CommonMark\Environment\ConfigurableEnvironmentInterface;
+use League\CommonMark\Environment\EnvironmentBuilderInterface;
 
 final class AlertExtension implements ExtensionInterface
 {
-    public function register(ConfigurableEnvironmentInterface $environment): void
+    public function register(EnvironmentBuilderInterface $environment): void
     {
         $environment
             ->addInlineParser(new AlertParser(), 20)
-            ->addInlineRenderer(Alert::class, new AlertRenderer(), 0);
+            ->addRenderer(Alert::class, new AlertRenderer(), 0);
     }
 }
