4ffc673cc4e44e356468706b5e1ee609b3015e04
amoschou <4948673+amoschou@users.noreply.github.com>
2023-09-23 13:47:42 +0930

    Update Alert.php

--- src/Alert.php
+++ src/Alert.php
@@ -2,7 +2,44 @@
 
 namespace AMoschou\CommonMark\Alert;
 
-class Alert
+use League\CommonMark\Node\Inline\AbstractInline;
+
+class Alert extends AbstractInline
 {
-    //
+    private string $type;
+
+    public function __construct(string $type)
+    {
+        parent::__construct();`
+
+        $this->setType($type);
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    public function getHeader()
+    {
+        return match ($this->getType()) {
+            'note' => 'Note',
+            'important' => 'Important',
+            'warning' => 'Warning',
+        };
+    }
+
+    public function getOcticon()
+    {
+        return match ($this->getType()) {
+            'note' => 'info',
+            'important' => 'report',
+            'warning' => 'alert',
+        };
+    }
 }
