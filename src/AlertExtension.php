25c6549ca683243037934b30075945599f5d0122
amoschou <4948673+amoschou@users.noreply.github.com>
2023-09-23 16:17:03 +0930

    Update AlertExtension.php

--- src/AlertExtension.php
+++ src/AlertExtension.php
@@ -10,7 +10,7 @@ final class AlertExtension implements ExtensionInterface
     public function register(EnvironmentBuilderInterface $environment): void
     {
         $environment
-            ->addInlineParser(new AlertParser(), 20)
+            ->addInlineParser(new AlertParser())
             ->addRenderer(Alert::class, new AlertRenderer(), 0);
     }
 }
